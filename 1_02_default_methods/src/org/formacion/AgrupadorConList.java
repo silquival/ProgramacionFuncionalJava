590afb2b3b569dcc00c1a975fba473383144b13c
silvia <silquival@gmail.com>
2021-08-23 17:19:32 +0200

    Primer commit hasta las seccion 3

---
+++ 1_02_default_methods/src/org/formacion/AgrupadorConList.java
@@ -0,0 +1,22 @@
+package org.formacion;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AgrupadorConList implements Agrupador {
+
+	private List<Object> contenido = new ArrayList<>();
+
+	@Override
+	public void add(Object elemento) {
+		contenido.add(elemento);
+	}
+
+	@Override
+	public int numeroElementos() {
+		return contenido.size();
+	}
+	
+	// todo implementad los metodos de la interface usando la lista contenido
+
+}
